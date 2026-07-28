6a1cf2cf40d587df696295e7351baa5766de9285
alka86308-cpu <alka86308@gmail.com>
2026-07-28 20:30:49 +0800

    Implement apiPost function for API interactions
    Added async function to handle API POST requests.

--- js/api.js
+++ js/api.js
@@ -1,6 +1,35 @@
-/******************************************************
+/
+******************************************************
  * API CONFIG
  ******************************************************/
 
 const API_URL =
 "https://script.google.com/macros/s/AKfycbyLkrZB_2_sbn3qrjqDN6aGN3QxS8VcvvvWkDX15Ll3jrgpiurExvxKUWQ5_knZpZQN/exec";
+/
+******************************************************
+ * API POST
+ ******************************************************/
+
+async function apiPost(action, data = {}) {
+
+    const formData = new FormData();
+
+    formData.append("action", action);
+
+    Object.keys(data).forEach(function(key){
+
+        formData.append(key, data[key]);
+
+    });
+
+    const response = await fetch(API_URL, {
+
+        method: "POST",
+
+        body: formData
+
+    });
+
+    return await response.json();
+
+}
