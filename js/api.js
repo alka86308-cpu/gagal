4cf2d1766c443e233c872e3719ade2924b1ef7ce
alka86308-cpu <alka86308@gmail.com>
2026-07-28 20:28:39 +0800

    Add API configuration in api.js

---
+++ js/api.js
@@ -0,0 +1,6 @@
+/******************************************************
+ * API CONFIG
+ ******************************************************/
+
+const API_URL =
+"https://script.google.com/macros/s/AKfycbyLkrZB_2_sbn3qrjqDN6aGN3QxS8VcvvvWkDX15Ll3jrgpiurExvxKUWQ5_knZpZQN/exec";
