f5909f9a0e8aed3f4ba639ce6b837662bc33ed04
alka86308-cpu <alka86308@gmail.com>
2026-07-28 21:55:53 +0800

    Update login.js

--- js/login.js
+++ js/login.js
@@ -22,61 +22,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
 async function login() {
     alert("Login dijalankan");
-
-    const username = document.getElementById("username").value.trim();
-    const password = document.getElementById("password").value.trim();
-
-    if (!username || !password) {
-        showMessage("Username dan Password wajib diisi", "red");
-        return;
-    }
-
-    showLoading();
-    console.log("Mulai login...");
-console.log("Username:", username);
-
-    try {
-
-        const result = await apiPost("login", {
-            username: username,
-            password: password
-        });
-
-        hideLoading();
-
-        if (result.status) {
-
-            localStorage.setItem("TOKEN", result.token);
-            localStorage.setItem("ROLE", result.role);
-            localStorage.setItem("NAMA", result.nama);
-
-            showMessage("Login berhasil", "green");
-
-            setTimeout(function () {
-                window.location.href = "dashboard.html";
-            }, 800);
-
-        } else {
-
-            showMessage(result.message || "Login gagal", "red");
-
-        }
-
-    } catch (err) {
-
-        hideLoading();
-        alert(err.message);
-console.log(err);
-
-        showMessage("Tidak dapat terhubung ke server", "red");
-
-        console.error(err);
-
-    }
-
+return;
 }
-
-
 /*************************************************
  * CEK LOGIN
  *************************************************/
